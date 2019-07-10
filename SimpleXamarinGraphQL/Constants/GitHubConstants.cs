36b9b816f92b1768c0a92ed413a004f03d403cde
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-07-10 07:56:59 -0700

    Added GitHubConstants

---
+++ SimpleXamarinGraphQL/Constants/GitHubConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleXamarinGraphQL
+{
+    public static class GitHubConstants
+    {
+#error Missing Token, Follow these steps to create your Personal Access Token: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/#creating-a-token
+        public const string PersonalAccessToken = "";
+
+        public const string GraphQLApiUrl = "https://api.github.com/graphql";
+    }
+}
