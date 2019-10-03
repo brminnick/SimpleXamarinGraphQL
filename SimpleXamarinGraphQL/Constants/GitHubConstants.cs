844583369b9fa8b0b6f5bd94be41e51efb900944
Michael Staib <michael.staib@hotmail.com>
2019-10-04 00:21:39 +0200

    started adding strawberry shake

--- SimpleXamarinGraphQL/Constants/GitHubConstants.cs
+++ SimpleXamarinGraphQL/Constants/GitHubConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GitHubConstants
     {
-#error Missing Token, Follow these steps to create your Personal Access Token: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/#creating-a-token
+
         public const string PersonalAccessToken = "";
 
         public const string GraphQLApiUrl = "https://api.github.com/graphql";
